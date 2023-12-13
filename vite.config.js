f97fdb2dfb18694d366b1d058166516b46655e16
Toy Santana António <toysan3@hotmail.com>
2023-12-13 14:18:31 +0100

    Update

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Base public path setup for GitHub Pages
+  base: '/CV-WebGl/',
+
+  // Other configurations can be added here as needed
+});
